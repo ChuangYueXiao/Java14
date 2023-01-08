d886bb7a51775ef7b47d831bc45ddadf60f680cf
ChuangYueXiao <116137081+ChuangYueXiao@users.noreply.github.com>
2023-01-08 16:23:33 +0800

    Update hw14_03.java

--- hw14_03.java
+++ hw14_03.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-public class hw14_3
+public class hw14_03
 {
    public static void main(String args[]) throws IOException
    {
